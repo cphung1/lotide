8abe65cb18156c7ec68d66fcbc75fce9c95f1a33
cphung1 <cphung97@gmail.com>
2020-02-04 23:36:56 +0000

    added more test cases

--- eqArrays.js
+++ eqArrays.js
@@ -18,6 +18,10 @@ const eqArrays = function(arrayOne, arrayTwo) {
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+assertEqual(eqArrays(['one', 2, 3], [1, 2, 3]), false);
+assertEqual(eqArrays(['1', '2', '3'], ['1', '2']), false);
