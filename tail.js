dfbd1ea5e3fcbbd5c09dda53779098a5610af810
cphung1 <cphung97@gmail.com>
2020-02-04 00:26:31 +0000

    returns tail of array aka all elements but first

---
+++ tail.js
@@ -0,0 +1,33 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+assertEqual();
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+// Test Case: An array with only one
+const word = ["Hello"];
+assertEqual(tail(word).length, 0); // should yield empty array for its tail
+
+// Test Case: Empty Array
+const empty = [];
+assertEqual(tail(empty).length, 0); // should yield empty array for its tail
