c09ea6488ab77563cfdfacf74908d7db21d488d3
cphung1 <cphung97@gmail.com>
2020-02-04 23:38:55 +0000

    added more test cases

--- eqArrays.js
+++ eqArrays.js
@@ -25,3 +25,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
 assertEqual(eqArrays(['one', 2, 3], [1, 2, 3]), false);
 assertEqual(eqArrays(['1', '2', '3'], ['1', '2']), false);
+assertEqual(eqArrays(['1', '2', '3'], [1, 2, 3]), false);
