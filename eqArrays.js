ff01cdd3b4ec0bfe099ec7a28a1cf3e08996b44c
cphung1 <cphung97@gmail.com>
2020-02-04 23:30:20 +0000

    eslint changes

--- eqArrays.js
+++ eqArrays.js
@@ -18,6 +18,6 @@ const eqArrays = function(arrayOne, arrayTwo) {
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); 
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
