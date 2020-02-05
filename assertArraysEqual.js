fe13a78699b755eaae866ad5bf7edd43e8643f5f
cphung1 <cphung97@gmail.com>
2020-02-05 00:19:44 +0000

    function to assert that two arrays are equal

---
+++ assertArraysEqual.js
@@ -0,0 +1,19 @@
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  const result = eqArrays(firstArray, secondArray) 
+  if (!result) {
+    console.log(`🛑🛑🛑 Arrays are not equal.`)
+  } else {
+    console.log(`✅✅✅ Arrays are equal.`)
+  }
+};
+
+assertArraysEqual(['1', 2, 3], [1, 2, 3]);
