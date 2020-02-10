45331a08905ce34657eb2626c5e88f9566d12fd0
cphung1 <cphung97@gmail.com>
2020-02-10 23:24:23 +0000

    initial commit of index.js, put all funcitons of lotide into an object

---
+++ index.js
@@ -0,0 +1,39 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const assertEqual = require('./assertEqual');
+const countLetters = require('./countLetters');
+const eqArrays = require('./eqArrays'); 
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUnitl = require('./takeUntil');
+const without = require('./without');
+const countOnly = require('./countOnly');
+
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertObjectsEqual,
+  assertEqual,
+  countLetters,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUnitl,
+  without,
+  countOnly
+};
