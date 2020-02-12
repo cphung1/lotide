4a17133d4f5b4ea40b3b9ee8c1fa65901af0cd15
cphung1 <cphung97@gmail.com>
2020-02-12 01:08:53 +0000

    made eqArrays recursive

--- eqArrays.js
+++ eqArrays.js
@@ -2,15 +2,28 @@
 const assertEqual = require('./assertEqual');
 
 const eqArrays = function(arrayOne, arrayTwo) {
+
   if (arrayOne.length !== arrayTwo.length) {
     return false;
   }
+
   for (let i = 0; i <= arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
+    if(Array.isArray(arrayOne[i]) && Array.isArray(arrayTwo[i])) {
+      if(!eqArrays(arrayOne[i], arrayTwo[i])){
+        return false;
+      }
+    } else if (arrayOne[i] !== arrayTwo[i]) {
       return false;
     }
   }
+
   return true;
+
 };
 
 module.exports = eqArrays;
+
+
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
--- eqObjects.js
+++ eqObjects.js
@@ -2,19 +2,41 @@ const eqArrays = require('./eqArrays');
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
-const eqObjects = function(object1, object2) {
-    if (Object.keys(object1).length !== Object.keys(object2).length) {
-      return false;
-    }
-    for (const val in object1) {
-      if (Array.isArray(object1[val]) && Array.isArray(object2[val])) {
-        return eqArrays(object1[val], object2[val]);
-      }
-      if (object1[val] !== object2[val]) {
-        return false;
-      }
-    }
-    return true;
-};
+// const eqObjects = function(object1, object2) {
+//     if (Object.keys(object1).length !== Object.keys(object2).length) {
+//       return false;
+//     }
+//     for (const val in object1) {
+//       if (Array.isArray(object1[val]) && Array.isArray(object2[val])) {
+//         return eqArrays(object1[val], object2[val]);
+//       }
+//       if (object1[val] !== object2[val]) {
+//         return false;
+//       }
+//     }
+//     return true;
+// };
 
 module.exports = eqObjects;
+
+const eqObjects = function(object1, object2) {
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const val in object1) {
+    if (Array.isArray(object1[val]) && Array.isArray(object2[val])) {
+      return eqArrays(object1[val], object2[val]);
+    }
+    if (object1[val] !== object2[val]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+console.log(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => true
+
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 })); // => false
+console.log(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }));
+ // => false
